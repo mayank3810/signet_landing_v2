e48ee4e2957c41bbb19dad18028e3e5e7ff149b1
Manish Gautam <manish.gautam.india@gmail.com>
2023-01-28 06:46:47 +0530

    Header complete

--- components/Header.js
+++ components/Header.js
@@ -30,247 +30,34 @@ function Header() {
             {/* MAIN MENU */}
             <nav className="wsmenu clearfix">
               <ul className="wsmenu-list nav-orange-red-hover">
-                {/* MEGAMENU */}
-                <li aria-haspopup="true" className="mg_link">
-                  <a href="#">
-                    Home <span className="wsarrow" />
-                  </a>
-                  <div className="wsmegamenu w-75 clearfix">
-                    <div className="container">
-                      <div className="row">
-                        {/* MEGAMENU LINKS */}
-                        <ul className="col-md-12 col-lg-3 link-list">
-                          <li className="fst-li">
-                            <a href="demo-1.html">App Landing</a>
-                          </li>
-                          <li>
-                            <a href="demo-2.html">App Showcase 1</a>
-                          </li>
-                          <li>
-                            <a href="demo-3.html">Startup Agency</a>
-                          </li>
-                          <li>
-                            <a href="demo-4.html">Design Agency</a>
-                          </li>
-                          <li>
-                            <a href="demo-5.html">Software 1</a>
-                          </li>
-                          <li>
-                            <a href="demo-6.html">SaaS Service 1</a>
-                          </li>
-                          <li>
-                            <a href="demo-7.html">Digital Service 1</a>
-                          </li>
-                        </ul>
-                        {/* MEGAMENU LINKS */}
-                        <ul className="col-md-12 col-lg-3 link-list">
-                          <li className="fst-li">
-                            <a href="demo-8.html">Social Media Marketing</a>
-                          </li>
-                          <li>
-                            <a href="demo-9.html">Digital Agency</a>
-                          </li>
-                          <li>
-                            <a href="demo-10.html">SaaS Service 2</a>
-                          </li>
-                          <li>
-                            <a href="demo-11.html">Desktop Software 1</a>
-                          </li>
-                          <li>
-                            <a href="demo-12.html">Digital Service 2</a>
-                          </li>
-                          <li>
-                            <a href="demo-13.html">Software SaaS</a>
-                          </li>
-                          <li>
-                            <a href="demo-14.html">App Showcase 2</a>
-                          </li>
-                        </ul>
-                        {/* MEGAMENU LINKS */}
-                        <ul className="col-md-12 col-lg-3 link-list">
-                          <li className="fst-li">
-                            <a href="demo-15.html">Software 2</a>
-                          </li>
-                          <li>
-                            <a href="demo-16.html">App Showcase 3</a>
-                          </li>
-                          <li>
-                            <a href="demo-17.html">Desktop Software 2</a>
-                          </li>
-                          <li>
-                            <a href="demo-18.html">SEO Company</a>
-                          </li>
-                          <li>
-                            <a href="demo-19.html">Digital Marketing</a>
-                          </li>
-                          <li>
-                            <a href="demo-20.html">Cyber Security</a>
-                          </li>
-                          <li>
-                            <a href="demo-21.html">SaaS Service 3</a>
-                          </li>
-                        </ul>
-                        {/* MEGAMENU LINKS */}
-                        <ul className="col-md-12 col-lg-3 link-list">
-                          <li className="fst-li">
-                            <a href="demo-22.html">Marketing Agency</a>
-                          </li>
-                          <li>
-                            <a href="demo-23.html">Branding Agency</a>
-                          </li>
-                          <li>
-                            <a href="404.html">404 Page</a>
-                          </li>
-                          <li>
-                            <a href="demo-24.html">RTL Version #1</a>
-                          </li>
-                          <li>
-                            <a href="demo-25.html">RTL Version #2</a>
-                          </li>
-                          <li>
-                            <a href="demo-26.html">RTL Version #3</a>
-                          </li>
-                        </ul>
-                      </div>{" "}
-                      {/* End row */}
-                    </div>{" "}
-                    {/* End container */}
-                  </div>{" "}
-                  {/* End wsmegamenu */}
-                </li>{" "}
-                {/* END MEGAMENU */}
-                {/* DROPDOWN MENU */}
-                <li aria-haspopup="true">
-                  <a href="#">
-                    About <span className="wsarrow" />
-                  </a>
-                  <ul className="sub-menu">
-                    <li aria-haspopup="true">
-                      <a href="#content-2">Why Signet?</a>
-                    </li>
-                    <li aria-haspopup="true">
-                      <a href="#content-5">Best Solutions</a>
-                    </li>
-                    <li aria-haspopup="true">
-                      <a href="#content-3">Integrations</a>
-                    </li>
-                    <li aria-haspopup="true">
-                      <a href="#content-10">How It Works</a>
-                    </li>
-                    <li aria-haspopup="true">
-                      <a href="#reviews-1">Testimonials</a>
-                    </li>
-                    <li aria-haspopup="true">
-                      <a href="#faqs-2">FAQs</a>
-                    </li>
-                  </ul>
-                </li>
-                {/* DROPDOWN MENU */}
-                <li aria-haspopup="true">
-                  <a href="#">
-                    Pages <span className="wsarrow" />
-                  </a>
-                  <div className="wsmegamenu clearfix halfmenu">
-                    <div className="container-fluid">
-                      <div className="row">
-                        {/* Links */}
-                        <ul className="col-lg-6 link-list">
-                          <li>
-                            <a href="about.html">About Us</a>
-                          </li>
-                          <li>
-                            <a href="features.html">Features &amp; Addons</a>
-                          </li>
-                          <li>
-                            <a href="pricing.html">Pricing Packages</a>
-                          </li>
-                          <li>
-                            <a href="download.html">Download Page</a>
-                          </li>
-                          <li>
-                            <a href="projects.html">Our Projects</a>
-                          </li>
-                          <li>
-                            <a href="project-details.html">Project Details</a>
-                          </li>
-                        </ul>
-                        {/* Links */}
-                        <ul className="col-lg-6 link-list">
-                          <li>
-                            <a href="team.html">Meet The Team</a>
-                          </li>
-                          <li>
-                            <a href="faqs.html">FAQs Page</a>
-                          </li>
-                          <li>
-                            <a href="blog-listing.html">Blog Listing</a>
-                          </li>
-                          <li>
-                            <a href="single-post.html">Single Blog Post</a>
-                          </li>
-                          <li>
-                            <a href="terms.html">Terms &amp; Privacy</a>
-                          </li>
-                          <li>
-                            <a href="contacts.html">Contact Us</a>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </li>{" "}
-                {/* END DROPDOWN MENU */}
-                {/* DROPDOWN MENU */}
-                <li aria-haspopup="true">
-                  <a href="#">
-                    Auth Pages <span className="wsarrow" />
-                  </a>
-                  <ul className="sub-menu">
-                    <li aria-haspopup="true">
-                      <a href="login-simple.html">
-                        Login Simple <span>NEW</span>
-                      </a>
-                    </li>
-                    <li aria-haspopup="true">
-                      <a href="login-boxed.html">
-                        Login Boxed <span>NEW</span>
-                      </a>
-                    </li>
-                    <li aria-haspopup="true">
-                      <a href="login-image.html">
-                        Login Image <span>NEW</span>
-                      </a>
-                    </li>
-                    <li aria-haspopup="true">
-                      <a href="signup-simple.html">
-                        Signup Simple <span>NEW</span>
-                      </a>
-                    </li>
-                    <li aria-haspopup="true">
-                      <a href="signup-boxed.html">
-                        Signup Boxed <span>NEW</span>
-                      </a>
-                    </li>
-                    <li aria-haspopup="true">
-                      <a href="signup-image.html">
-                        Signup Image <span>NEW</span>
-                      </a>
-                    </li>
-                    <li aria-haspopup="true">
-                      <a href="reset-password-1.html">
-                        Reset Pass. #1 <span>NEW</span>
-                      </a>
-                    </li>
-                    <li aria-haspopup="true">
-                      <a href="reset-password-2.html">
-                        Reset Pass. #2 <span>NEW</span>
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                {/* SIMPLE NAVIGATION LINK */}
                 <li className="nl-simple" aria-haspopup="true">
-                  <a href="#features-8">Features</a>
+                  <a href="#">Home</a>
+                </li>
+                <li aria-haspopup="true">
+                  <a href="#">
+                    Industries <span className="wsarrow" />
+                  </a>
+                  <ul className="sub-menu">
+                    <li aria-haspopup="true">
+                      <a href="#content-2">Agriculture</a>
+                    </li>
+                    <li aria-haspopup="true">
+                      <a href="#content-5">Pharma</a>
+                    </li>
+                    <li aria-haspopup="true">
+                      <a href="#content-10">Consumer Goods</a>
+                    </li>
+                    <li aria-haspopup="true">
+                      <a href="#reviews-1">Ticketing</a>
+                    </li>
+                    <li aria-haspopup="true">
+                      <a href="#faqs-2">Arts and Collectibles</a>
+                    </li>
+                  </ul>
+                </li>
+               
+                <li className="nl-simple" aria-haspopup="true">
+                  <a href="#features-8">Technology</a>
                 </li>
                 {/* SIMPLE NAVIGATION LINK */}
                 <li className="nl-simple" aria-haspopup="true">
@@ -278,20 +65,11 @@ function Header() {
                 </li>
                 {/* HEADER BUTTON */}
                 <li className="nl-simple" aria-haspopup="true">
-                  <a
-                    href="#content-4"
-                    className="btn btn-tra-white orange-red-hover last-link"
-                  >
-                    Get Started
+                  <a href="#content-4"
+                    className="btn btn-tra-white orange-red-hover last-link">
+                    Schedule a Demo
                   </a>
                 </li>
-                {/* HEADER SOCIAL LINKS 													
-                              <li class="nl-simple white-color header-socials ico-20 clearfix" aria-haspopup="true">
-                                  <span><a href="#" class="ico-facebook"><span class="flaticon-facebook"></span></a></span>
-                                  <span><a href="#" class="ico-twitter"><span class="flaticon-twitter"></span></a></span>
-                                  <span><a href="#" class="ico-instagram"><span class="flaticon-instagram"></span></a></span>
-                                  <span><a href="#" class="ico-dribbble"><span class="flaticon-dribbble"></span></a></span>	
-                              </li> */}
               </ul>
             </nav>{" "}
             {/* END MAIN MENU */}
--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -12,82 +12,42 @@ function Hero() {
   }, []);
 
   return (
-    <section id="hero-1" className="bg-scroll hero-section division">
-      <div className="container">
-        <div className="row d-flex align-items-center">
-          {/* HERO TEXT */}
-          <div className="col-md-7 col-lg-6">
-            <div
-              className="hero-1-txt white-color animate__animated animate__fadeInRight"
-              style={
-                addclass ? { visibility: "visible" } : { visibility: "hidden" }
-              }
-            >
-              {/* Title */}
-              <h2 className="h2-md">Authenticity and Customer Engagment</h2>
-              {/* Text */}
-              <p className="p-xl">
-                Signet tags links your products with blockchain-proven
-                ownership. The blockchain solution to ensure authenticity and
-                increase customer engagement.
-              </p>
-              {/* Buttons Group */}
-              <div className="btns-group mb-20">
-                <a
-                  href="#content-3"
-                  className="btn btn-orange-red tra-white-hover mr-15"
-                >
-                  Schedule a Demo
-                </a>
-                <a
-                  href="https://www.youtube.com/watch?v=YQUjE2koNRI"
-                  className="video-popup2 btn btn-md btn-transparent ico-20 ico-left"
-                >
-                  <span className="flaticon-play" /> See Signet in Action
-                </a>
-              </div>
-              {/* Advantages List */}
-              <ul className="advantages">
-                <li className="first-li">
-                  <p>14 days free trial</p>
-                </li>
-                <li className="last-li">
-                  <p>No credit card</p>
-                </li>
-              </ul>
-            </div>
-          </div>{" "}
-          {/* END HERO TEXT */}
-          {/* HERO IMAGE */}
-          <div className="col-md-5 col-lg-6">
-            <div
-              className="hero-1-img animate__animated animate__fadeInLeft"
-              style={
-                addclass ? { visibility: "visible" } : { visibility: "hidden" }
-              }
-            >
-              <img
-                className="img-fluid"
-                src="/images/hero-1-img.png"
-                alt="hero-image"
-              />
-            </div>
+    <section id="hero-23" className="bg-scroll hero-section division">
+    <div className="container">	
+      <div className="row d-flex align-items-center">
+        {/* HERO TEXT */}
+        <div className="col-lg-6">
+          <div className="hero-23-txt white-color wow fadeInRight" style={{visibility: 'visible', animationName: 'fadeInRight'}}>
+            {/* Title */}
+            <h2 className="h2-sm">
+              Anti counterfeit,<br/>
+              Authenticity and <br/>
+              Customer Engagment </h2> 
+            {/* Text */}
+            <p className="p-xl">
+            Signet tags links your products with blockchain-proven ownership. The blockchain solution to ensure authenticity and increase customer engagement.
+
+            </p>
+            {/* Button */}
+            <a href="#" className="btn btn-yellow tra-white-hover">Try Signet for Free</a>
           </div>
-        </div>{" "}
-        {/* End row */}
-      </div>{" "}
-      {/* End container */}
-      {/* WAVE SHAPE BOTTOM */}
-      <div className="wave-shape-bottom">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 130">
-          <path
-            fill="#ffffff"
-            fillOpacity={1}
-            d="M0,64L120,80C240,96,480,128,720,128C960,128,1200,96,1320,80L1440,64L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
-          />
-        </svg>
-      </div>
-    </section>
+        </div>	{/* END HERO TEXT */}	
+        {/* HERO IMAGE */}
+        <div className="col-lg-6">	
+          <div className="hero-23-img video-preview wow fadeInLeft" style={{visibility: 'visible', animationName: 'fadeInLeft'}}>	
+            {/* Play Icon */} 
+            <a className="video-popup1" href="https://www.youtube.com/embed/SZEflIVnhH8">				
+              <div className="video-btn video-btn-xl bg-deepyellow ico-90">	
+                <div className="video-block-wrapper"><span className="flaticon-play-button" /></div>
+              </div>
+            </a>
+            {/* Preview Image */} 
+            <img className="img-fluid hero-23-img" src="images/proof_authenticity.jpeg" alt="video-preview" />
+          </div>
+        </div>
+      </div>    {/* End row */} 	
+    </div>	   {/* End container */} 	
+  </section>
   );
 }
 
