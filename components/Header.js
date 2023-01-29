974ed3158bde656856308df2e14a90cde8dc5dd8
Manish Gautam <mayankgautam2105@gmail.com>
2023-01-29 13:40:24 +0530

    updates on header

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,32 @@
-import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
 function Header({ scroll }) {
+  const [activeRoute, setActiveRoute] = useState();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    fetchActiveRoute();
+    console.log(router.pathname);
+    // console.log(router.pathname.includes("feed"));
+  }, [router.pathname]);
+
+  function fetchActiveRoute() {
+    if (router.pathname.includes("blog")) {
+      setActiveRoute("blog");
+    } else if (router.pathname.includes("industries")) {
+      setActiveRoute("industries");
+    } else if (router.pathname.includes("technology")) {
+      setActiveRoute("technology");
+    } else if (router.pathname.includes("pricing")) {
+      setActiveRoute("pricing");
+    } else {
+      setActiveRoute("home");
+    }
+  }
+
   return (
     <header id="header" className="header tra-menu navbar-light">
       <div className="header-wrapper">
@@ -37,10 +63,19 @@ function Header({ scroll }) {
             <nav className="wsmenu clearfix">
               <ul className="wsmenu-list nav-orange-red-hover">
                 <li className="nl-simple" aria-haspopup="true">
-                  <a href="#">Home</a>
+                  <Link
+                    className={activeRoute === "home" ? "active-link" : ""}
+                    href={"/"}
+                  >
+                    Home
+                  </Link>
                 </li>
                 <li aria-haspopup="true">
-                  <a href="#">
+                  <a
+                    className={
+                      activeRoute === "industries" ? "active-link" : ""
+                    }
+                  >
                     Industries <span className="wsarrow" />
                   </a>
                   <ul className="sub-menu">
@@ -63,18 +98,35 @@ function Header({ scroll }) {
                 </li>
 
                 <li className="nl-simple" aria-haspopup="true">
-                  <a href="#features-8">Technology</a>
+                  <a
+                    className={
+                      activeRoute === "technology" ? "active-link" : ""
+                    }
+                  >
+                    Technology
+                  </a>
+                </li>
+                <li className="nl-simple" aria-haspopup="true">
+                  <Link
+                    className={activeRoute === "blog" ? "active-link" : ""}
+                    href={"/blog"}
+                  >
+                    Blog
+                  </Link>
+                  {/* <a href="#features-8"></a> */}
                 </li>
                 {/* SIMPLE NAVIGATION LINK */}
                 <li className="nl-simple" aria-haspopup="true">
-                  <a href="pricing.html">Pricing</a>
+                  <Link
+                    className={activeRoute === "pricing" ? "active-link" : ""}
+                    href={"/pricing"}
+                  >
+                    Pricing
+                  </Link>
                 </li>
                 {/* HEADER BUTTON */}
                 <li className="nl-simple" aria-haspopup="true">
-                  <a
-                    href="#content-4"
-                    className="btn btn-tra-white orange-red-hover last-link"
-                  >
+                  <a className="btn btn-tra-white orange-red-hover last-link">
                     Schedule a Demo
                   </a>
                 </li>
