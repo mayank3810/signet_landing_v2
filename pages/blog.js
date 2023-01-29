a724dc13ba0e9342f14d60297d7874798d047255
Manish Gautam <mayankgautam2105@gmail.com>
2023-01-29 13:25:22 +0530

    updates on blog

--- pages/blog.js
+++ pages/blog.js
@@ -9,9 +9,28 @@ function Blog(props) {
     // console.log(props.items);
   }, []);
 
+  const getParsedDate = (strDate) => {
+    var strSplitDate = String(strDate).split(" ");
+    var date = new Date(strSplitDate[0]);
+    // alert(date);
+    var dd = date.getDate();
+    var mm = date.getMonth() + 1; //January is 0!
+
+    var yyyy = date.getFullYear();
+    if (dd < 10) {
+      dd = "0" + dd;
+    }
+    if (mm < 10) {
+      mm = "0" + mm;
+    }
+    date = dd + "-" + mm + "-" + yyyy;
+    return date.toString();
+  };
+
   return (
     <>
       <Header scroll={true} />
+
       <section
         id="blog-page"
         className="bg-snow wide-50 inner-page-hero blog-page-section division"
@@ -32,21 +51,22 @@ function Blog(props) {
             {/* BLOG POSTS */}
             <div className="row">
               <div className="col gallery-items-list">
-                <div
-                  className="masonry-wrap grid-loaded"
-                  style={{ position: "relative", height: "1270.01px" }}
-                >
-                  {/* BLOG POST #1 */}
+                <div className="masonry-wrap grid-loaded d-flex">
                   {props?.items.map((post) => (
                     <div key={post.id} className="blog-3-post masonry-image">
                       {/* BLOG POST IMAGE */}
                       <div className="blog-post-img">
                         <div className="hover-overlay">
-                          <img
-                            className="img-fluid"
-                            src="images/blog/post-1-img.jpg"
-                            alt="blog-post-image"
-                          />
+                          <Link href={"/blog/" + post?.data?.slug.iv}>
+                            <img
+                              className="img-fluid"
+                              src={
+                                "https://cloud.squidex.io/api/assets/signetblog/" +
+                                post?.data?.image?.iv[0]
+                              }
+                              alt="blog-post-image"
+                            />
+                          </Link>
                           <div className="item-overlay" />
                         </div>
                       </div>
@@ -54,7 +74,9 @@ function Blog(props) {
                       <div className="blog-post-txt">
                         {/* Post Tag */}
                         <p className="p-md post-tag">
-                          OLMO News  |  JuLy 31, 2021
+                          {post?.data?.postType.iv +
+                            " | " +
+                            getParsedDate(post.created)}
                         </p>
                         {/* Post Link */}
                         <h5 className="h5-md">
@@ -63,14 +85,10 @@ function Blog(props) {
                           </Link>
                           {/* <a href="single-post.html"></a> */}
                         </h5>
-                        {/* Post Meta */}
-                        <div className="post-meta">
-                          <p className="p-md">9 Comments</p>
-                        </div>
-                      </div>{" "}
-                      {/* END BLOG POST TEXT */}
+                      </div>
                     </div>
                   ))}
+                  {/* END BLOG POST #1 */}
                 </div>
               </div>
             </div>{" "}
