8e708a00a91b0283f98949fd2e6bf3328635c238
Abishek Bardewa <abishek@distinctcloud.io>
2023-03-02 18:15:27 +0530

    Updates

--- components/Benefits.js
+++ components/Benefits.js
@@ -1,170 +1,133 @@
-import React from "react";
+import React from 'react';
 
 function Benefits() {
-  return (
-    <section id="features-4" className="pb-60 features-section division">
-      <div className="container">
-        {/* FEATURES-4 WRAPPER */}
-        <div className="fbox-4-wrapper fbox-4-wide">
-          <div className="row row-cols-1 row-cols-md-2">
-            {/* FEATURE BOX #1 */}
-            <div className="col">
-              <div className="fbox-4 pr-25 mb-40 wow fadeInUp">
-                {/* Icon */}
-                <div className="fbox-ico">
-                  <div className="ico-65 shape-ico dark-color">
-                    <img
-                      className="ico-bkg"
-                      src="/images/ico-bkg.png"
-                      alt="ico-bkg"
-                    />
-                   <img src="/images/industries/icons8-connected-100.png" />
-                  </div>
-                </div>
-                {/* Text */}
-                <div className="fbox-txt">
-                  {/* Title */}
-                  <h5 className="h5-md">Digitally Connected Products</h5>
-                  {/* Text */}
-                  <p className="p-lg">
-                    Every product becomes digitally connected
-                  </p>
-                </div>
-              </div>
-            </div>
-            {/* FEATURE BOX #2 */}
-            <div className="col">
-              <div className="fbox-4 pl-25 mb-40 wow fadeInUp">
-                {/* Icon */}
-                <div className="fbox-ico">
-                  <div className="ico-65 shape-ico dark-color">
-                    <img
-                      className="ico-bkg"
-                      src="/images/ico-bkg.png"
-                      alt="ico-bkg"
-                    />
-                   <img src="/images/industries/icons8-combo-chart-100.png" />
-
-                  </div>
-                </div>
-                {/* Text */}
-                <div className="fbox-txt">
-                  {/* Title */}
-                  <h5 className="h5-md">Valuabe Marketing Insights</h5>
-                  {/* Text */}
-                  <p className="p-lg">
-                    Capture valuable marketing insights and consented first-party data
-                  </p>
-                </div>
-              </div>
-            </div>
-            {/* FEATURE BOX #3 */}
-            <div className="col">
-              <div className="fbox-4 pr-25 mb-40 wow fadeInUp">
-                {/* Icon */}
-                <div className="fbox-ico">
-                  <div className="ico-65 shape-ico dark-color">
-                    <img
-                      className="ico-bkg"
-                      src="/images/ico-bkg.png"
-                      alt="ico-bkg"
-                    />
-                    <img src="/images/industries/icons8-knight-shield-100.png" />
-                  </div>
-                </div>
-                {/* Text */}
-                <div className="fbox-txt">
-                  {/* Title */}
-                  <h5 className="h5-md">Protect your Brand</h5>
-                  {/* Text */}
-                  <p className="p-lg">
-                    Drive out counterfeited perfumes from the market and safeguard your customers’ health and money
-                  </p>
-                </div>
-              </div>
-            </div>
-            {/* FEATURE BOX #4 */}
-            <div className="col">
-              <div className="fbox-4 pl-25 mb-40 wow fadeInUp">
-                {/* Icon */}
-                <div className="fbox-ico">
-                  <div className="ico-65 shape-ico dark-color">
-                    <img
-                      className="ico-bkg"
-                      src="/images/ico-bkg.png"
-                      alt="ico-bkg"
-                    />
-                      <img src="/images/industries/icons8-opera-glasses-100.png" />
-                  </div>
-                </div>
-                {/* Text */}
-                <div className="fbox-txt">
-                  {/* Title */}
-                  <h5 className="h5-md">Montitor Products</h5>
-                  {/* Text */}
-                  <p className="p-lg">
-                    Track your products until the final customer. Detect product infiltrations and original stolen products
-                  </p>
-                </div>
-              </div>
-            </div>
-            {/* FEATURE BOX #5 */}
-            <div className="col">
-              <div className="fbox-4 pr-25 mb-40 wow fadeInUp">
-                {/* Icon */}
-                <div className="fbox-ico">
-                  <div className="ico-65 shape-ico dark-color">
-                    <img
-                      className="ico-bkg"
-                      src="/images/ico-bkg.png"
-                      alt="ico-bkg"
-                    />
-                      <img src="/images/industries/icons8-defend-100.png" />
-                  </div>
-                </div>
-                {/* Text */}
-                <div className="fbox-txt">
-                  {/* Title */}
-                  <h5 className="h5-md">Defend Distributors</h5>
-                  {/* Text */}
-                  <p className="p-lg">
-                    Protect your official commercial partners from product diversion and illicit markets
-                  </p>
-                </div>
-              </div>
-            </div>
-            {/* FEATURE BOX #6 */}
-            <div className="col">
-              <div className="fbox-4 pl-25 mb-40 wow fadeInUp">
-                {/* Icon */}
-                <div className="fbox-ico">
-                  <div className="ico-65 shape-ico dark-color">
-                    <img
-                      className="ico-bkg"
-                      src="/images/ico-bkg.png"
-                      alt="ico-bkg"
-                    />
-                    <img src="/images/industries/icons8-no-filling-100.png" />
-                  </div>
-                </div>
-                {/* Text */}
-                <div className="fbox-txt">
-                  {/* Title */}
-                  <h5 className="h5-md">Detect fraudulent refilling</h5>
-                  {/* Text */}
-                  <p className="p-lg">
-                   Easily detect if the products have been tampered, using tag tamper detecting tags.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>{" "}
-        {/* END FEATURES-4 WRAPPER */}
-      </div>{" "}
-      {/* End container */}
-    </section>
-  );
+	return (
+		<section id="features-4" className="pb-60 features-section division">
+			<div className="container">
+				{/* FEATURES-4 WRAPPER */}
+				<div className="fbox-4-wrapper fbox-4-wide">
+					<div className="row row-cols-1 row-cols-md-2">
+						{/* FEATURE BOX #1 */}
+						<div className="col">
+							<div className="fbox-4 pr-25 mb-40 wow fadeInUp">
+								{/* Icon */}
+								<div className="fbox-ico">
+									<div className="ico-65 shape-ico dark-color">
+										<img className="ico-bkg" src="/images/ico-bkg.png" alt="ico-bkg" />
+										<img src="/images/industries/icons8-connected-100.png" alt="ico-bkg" />
+									</div>
+								</div>
+								{/* Text */}
+								<div className="fbox-txt">
+									{/* Title */}
+									<h5 className="h5-md">Digitally Connected Products</h5>
+									{/* Text */}
+									<p className="p-lg">Every product becomes digitally connected</p>
+								</div>
+							</div>
+						</div>
+						{/* FEATURE BOX #2 */}
+						<div className="col">
+							<div className="fbox-4 pl-25 mb-40 wow fadeInUp">
+								{/* Icon */}
+								<div className="fbox-ico">
+									<div className="ico-65 shape-ico dark-color">
+										<img className="ico-bkg" src="/images/ico-bkg.png" alt="ico-bkg" />
+										<img src="/images/industries/icons8-combo-chart-100.png" alt="ico-bkg" />
+									</div>
+								</div>
+								{/* Text */}
+								<div className="fbox-txt">
+									{/* Title */}
+									<h5 className="h5-md">Valuabe Marketing Insights</h5>
+									{/* Text */}
+									<p className="p-lg">Capture valuable marketing insights and consented first-party data</p>
+								</div>
+							</div>
+						</div>
+						{/* FEATURE BOX #3 */}
+						<div className="col">
+							<div className="fbox-4 pr-25 mb-40 wow fadeInUp">
+								{/* Icon */}
+								<div className="fbox-ico">
+									<div className="ico-65 shape-ico dark-color">
+										<img className="ico-bkg" src="/images/ico-bkg.png" alt="ico-bkg" />
+										<img src="/images/industries/icons8-knight-shield-100.png" alt="ico-bkg" />
+									</div>
+								</div>
+								{/* Text */}
+								<div className="fbox-txt">
+									{/* Title */}
+									<h5 className="h5-md">Protect your Brand</h5>
+									{/* Text */}
+									<p className="p-lg">Drive out counterfeited perfumes from the market and safeguard your customers’ health and money</p>
+								</div>
+							</div>
+						</div>
+						{/* FEATURE BOX #4 */}
+						<div className="col">
+							<div className="fbox-4 pl-25 mb-40 wow fadeInUp">
+								{/* Icon */}
+								<div className="fbox-ico">
+									<div className="ico-65 shape-ico dark-color">
+										<img className="ico-bkg" src="/images/ico-bkg.png" alt="ico-bkg" />
+										<img src="/images/industries/icons8-opera-glasses-100.png" alt="ico-bkg" />
+									</div>
+								</div>
+								{/* Text */}
+								<div className="fbox-txt">
+									{/* Title */}
+									<h5 className="h5-md">Montitor Products</h5>
+									{/* Text */}
+									<p className="p-lg">Track your products until the final customer. Detect product infiltrations and original stolen products</p>
+								</div>
+							</div>
+						</div>
+						{/* FEATURE BOX #5 */}
+						<div className="col">
+							<div className="fbox-4 pr-25 mb-40 wow fadeInUp">
+								{/* Icon */}
+								<div className="fbox-ico">
+									<div className="ico-65 shape-ico dark-color">
+										<img className="ico-bkg" src="/images/ico-bkg.png" alt="ico-bkg" />
+										<img src="/images/industries/icons8-defend-100.png" alt="ico-bkg" />
+									</div>
+								</div>
+								{/* Text */}
+								<div className="fbox-txt">
+									{/* Title */}
+									<h5 className="h5-md">Defend Distributors</h5>
+									{/* Text */}
+									<p className="p-lg">Protect your official commercial partners from product diversion and illicit markets</p>
+								</div>
+							</div>
+						</div>
+						{/* FEATURE BOX #6 */}
+						<div className="col">
+							<div className="fbox-4 pl-25 mb-40 wow fadeInUp">
+								{/* Icon */}
+								<div className="fbox-ico">
+									<div className="ico-65 shape-ico dark-color">
+										<img className="ico-bkg" src="/images/ico-bkg.png" alt="ico-bkg" />
+										<img src="/images/industries/icons8-no-filling-100.png" alt="ico-bkg" />
+									</div>
+								</div>
+								{/* Text */}
+								<div className="fbox-txt">
+									{/* Title */}
+									<h5 className="h5-md">Detect fraudulent refilling</h5>
+									{/* Text */}
+									<p className="p-lg">Easily detect if the products have been tampered, using tag tamper detecting tags.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>{' '}
+				{/* END FEATURES-4 WRAPPER */}
+			</div>{' '}
+			{/* End container */}
+		</section>
+	);
 }
 
 export default Benefits;
