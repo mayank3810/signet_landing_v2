bd4d68c56597bf600f825f7fe3559c15dab1d7e5
Manish Gautam <manish.gautam.india@gmail.com>
2023-03-11 20:18:36 +0530

    Plarigism check

--- components/Home/CustomerEngagement.js
+++ components/Home/CustomerEngagement.js
@@ -2,40 +2,46 @@ import React, { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 function CustomerEngagement() {
-	const [containerRef, inContainerView] = useInView({ threshold: 0.2 });
-	const [init, setInit] = useState(false);
-	useEffect(() => {
-		if (!init && inContainerView) {
-			setInit(true);
-		}
-	}, [inContainerView]);
+  const [containerRef, inContainerView] = useInView({ threshold: 0.2 });
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    if (!init && inContainerView) {
+      setInit(true);
+    }
+  }, [inContainerView]);
 
-	return (
-		<section ref={containerRef} id="content-6" className="content-6 pb-60 pt-60 content-section division">
-			<div className="container">
-				<div className="row d-flex align-items-center">
-					{/* TEXT BLOCK */}
-					<div className="col-md-6 col-lg-6">
-						<div className="txt-block left-column wow fadeInRight">
-							{/* Title */}
-							<h2 className="h2-xs">Increase customer engagement by 100X</h2>
-							{/* Text */}
-							<p className="p-lg">
-								Signet tags with direct NFC connection allows an immediate check of product authenticity and integrity. An instant interaction with
-								the product grants access to its history, characteristics and sustainability certifications. Faster and safer than any solution based
-								on QR code or image recognition.
-							</p>
-						</div>
-					</div>
-					{/* IMAGE BLOCK */}
-					<div className="col-md-6 col-lg-6">
-						<div className="img-block right-column wow fadeInLeft">
-							{/* <img
+  return (
+    <section ref={containerRef} id="content-6" className="content-6 pb-60 pt-60 content-section division">
+      <div className="container">
+        <div className="row d-flex align-items-center">
+          <div className="col-md-6 col-lg-6">
+            <div className="txt-block left-column wow fadeInRight">
+              <h2 className="h2-xs">Increase customer engagement by 100X</h2>
+
+              <p className="p-lg">
+                Engage with your consumers like never before with Signet Tags. With our innovative solution, you'll always be able to keep track of your customers and never lose touch again.
+              </p>
+
+              <p className="p-lg">
+                Signet Tags directly triggers consumer engagement. 
+                Consumers can use their smartphones to scan products at the point of sale and, once connected to our dashboard, you can use those interactions to create valuable consumer profiles. These profiles will help your brand deliver targeted marketing initiatives.
+                
+              </p>
+
+             
+
+            
+            </div>
+          </div>
+          {/* IMAGE BLOCK */}
+          <div className="col-md-6 col-lg-6">
+            <div className="img-block right-column wow fadeInLeft">
+              {/* <img
                 className="img-fluid"
                 src="/images/img-20.png"
                 alt="content-image"
               /> */}
-							{/* <div className="masonry-container">
+              {/* <div className="masonry-container">
                 <div className="masonry-custom ">
 
                   <div className="brick height-100"></div>
@@ -102,15 +108,15 @@ function CustomerEngagement() {
                 </div>
               </div> */}
 
-							<img className="img-fluid" src="images/img-20.webp" alt="customers-image" />
-						</div>
-					</div>
-				</div>{' '}
-				{/* End row */}
-			</div>{' '}
-			{/* End container */}
-		</section>
-	);
+              <img className="img-fluid" src="images/img-20.webp" alt="customers-image" />
+            </div>
+          </div>
+        </div>{' '}
+        {/* End row */}
+      </div>{' '}
+      {/* End container */}
+    </section>
+  );
 }
 
 export default CustomerEngagement;
