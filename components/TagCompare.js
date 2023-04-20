d6cb955903fa24f19ca232c645a7fe422b7cbf8e
Manish Gautam <mayankgautam2105@gmail.com>
2023-04-20 16:15:30 +0530

    updates

---
+++ components/TagCompare.js
@@ -0,0 +1,272 @@
+import React from "react";
+
+function TagCompare() {
+  return (
+    <>
+      <div className="pricing-compare pc-40">
+        <div className="row">
+          <div className="col">
+            {/* Title */}
+            <h3 className="h3-sm text-center">
+              Signet Tamper Tags vs <br /> Traditional Tamper Tags
+            </h3>
+            {/* Table */}
+            <div className="table-responsive">
+              <table className="table text-center">
+                <thead>
+                  <tr>
+                    <th style={{ width: "40%" }} />
+                    <th style={{ width: "30%" }}>Traditional Tags</th>
+                    <th style={{ width: "30%" }}>Signet Tags</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row" className="text-start">
+                      Anti Clone DNA 424 NFC Tags
+                    </th>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/cancel.svg"
+                        alt="check"
+                      />
+                    </td>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/ok.svg"
+                        alt="check"
+                      />
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th scope="row" className="text-start">
+                      Tag Tamper Detection
+                    </th>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/cancel.svg"
+                        alt="check"
+                      />
+                    </td>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/ok.svg"
+                        alt="check"
+                      />
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th scope="row" className="text-start">
+                      User Location Detection
+                    </th>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/cancel.svg"
+                        alt="check"
+                      />
+                    </td>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/ok.svg"
+                        alt="check"
+                      />
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th scope="row" className="text-start">
+                      Colors and Theme Customisation
+                    </th>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/cancel.svg"
+                        alt="check"
+                      />
+                    </td>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/ok.svg"
+                        alt="check"
+                      />
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th scope="row" className="text-start">
+                      Advanced Analytics Dashboard
+                    </th>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/cancel.svg"
+                        alt="check"
+                      />
+                    </td>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/ok.svg"
+                        alt="check"
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row" className="text-start">
+                      Customer Push Notification
+                    </th>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/cancel.svg"
+                        alt="check"
+                      />
+                    </td>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/ok.svg"
+                        alt="check"
+                      />
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th scope="row" className="text-start">
+                      Feedback and Surveys
+                    </th>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/cancel.svg"
+                        alt="check"
+                      />
+                    </td>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/ok.svg"
+                        alt="check"
+                      />
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th scope="row" className="text-start">
+                      Track user location
+                    </th>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/cancel.svg"
+                        alt="check"
+                      />
+                    </td>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/ok.svg"
+                        alt="check"
+                      />
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th scope="row" className="text-start">
+                      Insights on Tag Users
+                    </th>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/cancel.svg"
+                        alt="check"
+                      />
+                    </td>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/ok.svg"
+                        alt="check"
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row" className="text-start">
+                      Run Contests and Offers
+                    </th>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/cancel.svg"
+                        alt="check"
+                      />
+                    </td>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/ok.svg"
+                        alt="check"
+                      />
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th scope="row" className="text-start">
+                      Multiple Language Support
+                    </th>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/cancel.svg"
+                        alt="check"
+                      />
+                    </td>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/ok.svg"
+                        alt="check"
+                      />
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th scope="row" className="text-start">
+                      White label Solution
+                    </th>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/cancel.svg"
+                        alt="check"
+                      />
+                    </td>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/ok.svg"
+                        alt="check"
+                      />
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>{" "}
+            {/* End Table */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TagCompare;
