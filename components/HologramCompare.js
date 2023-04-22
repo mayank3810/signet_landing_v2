3dc4d8916935d3a315f59de40b5269b18b94f03e
Manish Gautam <manish.gautam.india@gmail.com>
2023-04-22 13:59:34 +0530

    Updates

---
+++ components/HologramCompare.js
@@ -0,0 +1,196 @@
+import React from "react";
+
+function HologramCompare() {
+  return (
+    <>
+      <div className="pricing-compare pc-40">
+        <div className="row">
+          <div className="col">
+            {/* Title */}
+            <h3 className="h3-sm text-center">
+              Signet Hologram Tags vs <br /> Traditional Hologram
+            </h3>
+            {/* Table */}
+            <div className="table-responsive">
+              <table className="table text-center">
+                <thead>
+                  <tr>
+                    <th style={{ width: "40%" }} />
+                    <th style={{ width: "30%" }}>Other Hologram</th>
+                    <th style={{ width: "30%" }}>Signet Tags</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row" className="text-start">
+                      Anti counterfeiting QR code
+                    </th>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/cancel.svg"
+                        alt="check"
+                      />
+                    </td>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/ok.svg"
+                        alt="check"
+                      />
+                    </td>
+                  </tr>
+
+
+                  <tr>
+                    <th scope="row" className="text-start">
+                      User Location Detection
+                    </th>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/cancel.svg"
+                        alt="check"
+                      />
+                    </td>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/ok.svg"
+                        alt="check"
+                      />
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th scope="row" className="text-start">
+                      Increase repeat sales
+                    </th>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/cancel.svg"
+                        alt="check"
+                      />
+                    </td>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/ok.svg"
+                        alt="check"
+                      />
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th scope="row" className="text-start">
+                     Realtime Analytics
+                    </th>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/cancel.svg"
+                        alt="check"
+                      />
+                    </td>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/ok.svg"
+                        alt="check"
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row" className="text-start">
+                      Sned messages to customers
+                    </th>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/cancel.svg"
+                        alt="check"
+                      />
+                    </td>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/ok.svg"
+                        alt="check"
+                      />
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th scope="row" className="text-start">
+                      Feedback and Surveys
+                    </th>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/cancel.svg"
+                        alt="check"
+                      />
+                    </td>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/ok.svg"
+                        alt="check"
+                      />
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th scope="row" className="text-start">
+                      Warranty management system
+                    </th>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/cancel.svg"
+                        alt="check"
+                      />
+                    </td>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/ok.svg"
+                        alt="check"
+                      />
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <th scope="row" className="text-start">
+                      Run Contests and Offers
+                    </th>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/cancel.svg"
+                        alt="check"
+                      />
+                    </td>
+                    <td className="ico-30 green-color">
+                      <img
+                        className=""
+                        src="/images/icons/ok.svg"
+                        alt="check"
+                      />
+                    </td>
+                  </tr>
+
+                 
+                </tbody>
+              </table>
+            </div>{" "}
+            {/* End Table */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HologramCompare;
