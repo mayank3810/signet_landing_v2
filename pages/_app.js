93675ec850e20c5d9c7684bb2715af60d89878c3
Abishek Bardewa <abishek@distinctcloud.io>
2023-10-20 15:34:03 +0530

    thank you page added

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,12 @@
-import Footer from "@/components/Footer";
+import Footer from '@/components/Footer';
 import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-  return (
-    <>
-      <Component {...pageProps} />;
-      {
-        router.pathname.toLowerCase().indexOf("onboarding") < 0 &&
-        <Footer />
-      }
-    </>
-  );
+	const router = useRouter();
+	return (
+		<>
+			<Component {...pageProps} />
+			{router.pathname.toLowerCase().indexOf('onboarding') < 0 && <Footer />}
+		</>
+	);
 }
